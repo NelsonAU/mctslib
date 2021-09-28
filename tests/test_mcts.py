e9d8ec2c2b746afbc66b9d836657ebe71d1f8170
David <dunleavyd14@gmail.com>
2021-09-28 19:19:58 -0400

    Fixed HRAVE, restructuring. need to add setup.py

---
+++ tests/test_mcts.py
@@ -0,0 +1,16 @@
+import random, math
+from envs.up_or_right import Node
+from mctslib import MCTS
+
+
+def test_mcts():
+	turn_limit = 100
+	mcts = MCTS(Node(0, 0))
+	for i in range(turn_limit):
+		node = mcts.move(rollout_depth=3, iters=5)
+		if i == turn_limit - 1:
+			print(node)
+
+
+if __name__ == "__main__":
+	test_mcts()
