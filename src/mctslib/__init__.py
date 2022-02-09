7269c1790691700e6b793ff254b9ea70033eff2a
David Dunleavy <dunleavyd14@gmail.com>
2022-02-08 23:38:23 -0500

    Fix MCTS helper function

--- src/mctslib/__init__.py
+++ src/mctslib/__init__.py
@@ -1,6 +1,8 @@
 from . import _mctslib
 
-MCTS = _mctslib.MCTS
-
+def MCTS(root, *args, **kwargs):
+    """TODO: for now, this is simple, but adding versions of MCTS that store a map of canonical
+    nodes make things more complicated"""
+    return _mctslib.MCTS(root, *args, **kwargs)
 
 __all__ = ("MCTS")
