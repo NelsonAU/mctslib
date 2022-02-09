e5da6ab1e4612985ef8d54bfe9405fd3e86aaa72
David Dunleavy <dunleavyd14@gmail.com>
2022-02-09 17:14:17 -0500

    Put envs in the right place, make algorithms take args by keyword

--- src/mctslib/_mctslib.cpp
+++ src/mctslib/_mctslib.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include <pybind11/pybind11.h>
 
 #include "algorithm/mcts.h"
@@ -22,13 +24,18 @@ PYBIND11_MODULE(_mctslib, m) {
     //          No way to infer this because the Settings default constructor cannot be deleted,
     //          and because of this the compiler will always choose to decide that the parameter
     //          pack is empty.
+    
 
     py::class_<PyTreeIterMCTS>(m, "tree_iters_MCTS")
         .def(py::init<py::object>())
-        .def("move", &PyTreeIterMCTS::move<uint, uint, double, double>);
+        .def("move", &PyTreeIterMCTS::move<uint, uint, double, double>, "",
+                py::arg("rollout_depth"), py::arg("exploration_weight") = sqrt(2),
+                py::arg("iters") = 0, py::arg("cpu_time") = 0);
 
     py::class_<PyTreeCPUMCTS>(m, "tree_cpu_time_MCTS")
         .def(py::init<py::object>())
-        .def("move", &PyTreeCPUMCTS::move<uint, uint, double, double>);
+        .def("move", &PyTreeCPUMCTS::move<uint, uint, double, double>, "",
+                py::arg("rollout_depth"), py::arg("exploration_weight") = sqrt(2),
+                py::arg("iters") = 0, py::arg("cpu_time") = 0);
 
 }
