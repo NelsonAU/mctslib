e5afc97f8bc35b1003c7979da880decdca6448c9
David Dunleavy <dunleavyd14@gmail.com>
2022-02-09 20:58:54 -0500

    test clang-format on _mctslib.cpp

--- src/mctslib/_mctslib.cpp
+++ src/mctslib/_mctslib.cpp
@@ -13,7 +13,8 @@
 using namespace mctslib;
 namespace py = pybind11;
 
-PYBIND11_MODULE(_mctslib, m) {
+PYBIND11_MODULE(_mctslib, m)
+{
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     //@NOTE:    Must specify template args to move that correspond to the settings of the algorithm.
@@ -25,12 +26,11 @@ PYBIND11_MODULE(_mctslib, m) {
     py::class_<PyTreeIterMCTS>(m, "tree_iters_MCTS")
         .def(py::init<py::object>())
         .def("move", &PyTreeIterMCTS::move<uint, uint, double>, "", py::kw_only(),
-                py::arg("rollout_depth"), py::arg("iters"), py::arg("exploration_weight") = sqrt(2));
+            py::arg("rollout_depth"), py::arg("iters"), py::arg("exploration_weight") = sqrt(2));
 
     using PyTreeCPUMCTS = PyAlg<TreeMCTS<PythonNode, decltype(mcts_cpu_move), CPUMCTSSettings>>;
     py::class_<PyTreeCPUMCTS>(m, "tree_cpu_time_MCTS")
         .def(py::init<py::object>())
         .def("move", &PyTreeCPUMCTS::move<uint, double, double>, "", py::kw_only(),
-                py::arg("rollout_depth"), py::arg("cpu_time"), py::arg("exploration_weight") = sqrt(2));
-
+            py::arg("rollout_depth"), py::arg("cpu_time"), py::arg("exploration_weight") = sqrt(2));
 }
