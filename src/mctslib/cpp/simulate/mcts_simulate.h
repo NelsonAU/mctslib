af1e7b7aae88e9161a8875981cfc16779b44bd60
David Dunleavy <dunleavyd14@gmail.com>
2022-03-01 14:10:29 -0500

    Comment out leaf parallelization code

--- src/mctslib/cpp/simulate/mcts_simulate.h
+++ src/mctslib/cpp/simulate/mcts_simulate.h
@@ -17,33 +17,33 @@ inline auto mcts_simulate = []<class Alg>(Alg& alg, std::shared_ptr<typename Alg
     return node.stats.evaluation;
 };
 
-inline auto threaded_mcts_simulate = []<class Alg>(Alg& alg, std::shared_ptr<typename Alg::Node> start) -> double {
-    // used for leaf parallelization
-    typename Alg::Node node = *start;
-    uint num_threads = alg.body.settings.thread_count;
-    std::vector<double> evals(num_threads, 0);
-    std::vector<std::thread> threads;
+/* inline auto threaded_mcts_simulate = []<class Alg>(Alg& alg, std::shared_ptr<typename Alg::Node> start) -> double { */
+/*     // used for leaf parallelization */
+/*     typename Alg::Node node = *start; */
+/*     uint num_threads = alg.body.settings.thread_count; */
+/*     std::vector<double> evals(num_threads, 0); */
+/*     std::vector<std::thread> threads; */
 
-    threads.reserve(alg.body.settings.thread_count);
+/*     threads.reserve(alg.body.settings.thread_count); */
 
-    for (double& eval : evals) {
-        threads.push_back(
-            std::thread(
-                [node](uint t_rollout_depth, double& t_eval) {
-                    for (uint i = 0; i < t_rollout_depth; i++) {
-                        if (node.is_terminal())
-                            break;
+/*     for (double& eval : evals) { */
+/*         threads.push_back( */
+/*             std::thread( */
+/*                 [node](uint t_rollout_depth, double& t_eval) { */
+/*                     for (uint i = 0; i < t_rollout_depth; i++) { */
+/*                         if (node.is_terminal()) */
+/*                             break; */
 
-                        node = node.random_child();
-                    }
+/*                         node = node.random_child(); */
+/*                     } */
 
-                    return node.stats.evaluation;
-                }));
-    }
+/*                     return node.stats.evaluation; */
+/*                 })); */
+/*     } */
 
-    for (std::thread& thread : threads)
-        thread.join();
+/*     for (std::thread& thread : threads) */
+/*         thread.join(); */
 
-    return std::reduce(evals.begin(), evals.end()) / num_threads;
-};
+/*     return std::reduce(evals.begin(), evals.end()) / num_threads; */
+/* }; */
 }
