9575f9cbc1da585488339a8e28c5bfe77308c393
David Dunleavy <dunleavyd14@gmail.com>
2022-04-01 01:37:00 -0400

    Try to fix clang issues again

--- src/mctslib/cpp/bindings/python_node.h
+++ src/mctslib/cpp/bindings/python_node.h
@@ -19,7 +19,7 @@ public:
     std::vector<std::shared_ptr<PythonNode>> children;
 
     template <typename... Args>
-    PythonNode(pybind11::object obj, Args... args) requires(std::is_same_v<decltype(Stats::action), uint>)
+    PythonNode(pybind11::object obj, Args... args) requires requires(Stats stats) {stats.action;}
         : object(obj)
         , stats(obj.attr("evaluation")().cast<double>(), obj.attr("action_id").cast<uint>(), args...)
     {
